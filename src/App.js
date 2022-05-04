72c250ade6cb3bdd1a08638bd1d9c7040587434d
iamthefastone <o.osisiogu@gmail.com>
2022-05-04 21:43:18 +0100

    Backup before major changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from "react"
+import Start from "../src/components/Start"
 import Questions from "../src/components/Questions"
 import "../src/style.css"
 
 export default function App() {
 
-    
-
     const [questions, setQuestions] = React.useState([])
-    console.log(questions)
+    const [fetchErr, setFetchErr] = React.useState(null)
 
-    function fetchData() {
-        console.log(`fetch data button working. Difficulty is ${formData.quizDifficulty} and category is ${formData.quizCategory}`)
-        fetch(`https://opentdb.com/api.php?amount=10&category=20&difficulty=easy`)
-        .then(res => res.json())
-        .then(data => setQuestions(data))
-        console.log(questions)
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`https://opentdb.com/api.php?amount=10&category=${formData.quizCategory}&difficulty=${formData.quizDifficulty}`)
+            if (!response.ok) throw Error("Oops! 🙊 Something went wrong. Did not receive expected data from the API")
+            const data = await response.json()
+            // console.log(data)
+            setQuestions(data)
+            setFetchErr(null)
+            console.log(questions)
+        } catch (err) {
+            setFetchErr(err.message)
+        }
+        console.log("button clicked")
     }
 
     const [formData, setFormData] = React.useState({
@@ -29,66 +35,40 @@ export default function App() {
         })
     }
 
+    let theQuestion = ""
+    let answersArr = []
+
+    function generateQuiz() {
+        questions.map(question => {
+            theQuestion = question.question
+            answersArr = [question.correct_answer, question.incorrect_answers[0], question.incorrect_answers[1], question.incorrect_answers[2]]
+        })
+        console.log(answersArr)
+    }
+
+    generateQuiz()
+        
+
+
+        // <Questions question={question.question}
+        // correctAnswer={question.correct_answer}
+        // incorrectAnswer1={question.incorrect_answers[0]}/> 
+        // <div className="question-container">
+        //         <h3 className="question">{question}</h3>
+        //         <div className="answers-container">
+        //             <p className="answers selected">Adiós</p>
+        //             <p className="answers correct">Hola</p>
+        //             <p className="answers deactive">Au Revoir</p>
+        //             <p className="answers wrong">Salir</p>
+        //         </div>
+        //         <hr />
+        //     </div>
+
     return (
         <main>
             <div className="app-container">
-                <section className="start-section">
-                    <div className="header-image">
-                        <h1>Obinna's Trivia Quiz</h1>
-                        <p>Fun trivia games</p>
-                    </div>
-                    
-                    <form>
-                        <select 
-                            id="quizCategory"
-                            value={formData.quizCategory}
-                            onChange={handleChange}
-                            name="quizCategory"
-                        >
-                            <option value="">-- Choose category --</option>
-                            <option value="9">General Knowledge</option>
-                            <option value="10">Entertainment: Books</option>
-                            <option value="11">Entertainment: Film</option>
-                            <option value="12">Entertainment: Music</option>
-                            <option value="13">Entertainment: Musicals and Theatres</option>
-                            <option value="14">Entertainment: Television</option>
-                            <option value="15">Video Games</option>
-                            <option value="16">Board Games</option>
-                            <option value="17">Science and Nature</option>
-                            <option value="18">Science: Computers</option>
-                            <option value="19">Science: Mathematics</option>
-                            <option value="20">Mythology</option>
-                            <option value="21">Sports</option>
-                            <option value="22">Geography</option>
-                            <option value="23">History</option>
-                            <option value="24">Politics</option>
-                            <option value="25">Arts</option>
-                            <option value="26">Celebrities</option>
-                            <option value="27">Animals</option>
-                            <option value="28">Vehicles</option>
-                            <option value="29">Entertainment: Comics</option>
-                            <option value="30">Science: Gadgets</option>
-                            <option value="31">Entertainment: Japanese Anime and Manga</option>
-                            <option value="32">Entertainment: Cartoon and Animations</option>
-                        </select>
-                        <br />
-                        <select 
-                            id="quizDifficulty"
-                            value={formData.quizDifficulty}
-                            onChange={handleChange}
-                            name="quizDifficulty"
-                        >
-                            <option value="">-- Choose difficulty --</option>
-                            <option value="easy">Easy 😃</option>
-                            <option value="medium">Medium 😅</option>
-                            <option value="hard">Hard 🥵</option>
-                        </select>
-                    </form>
-
-                    <button className="start-btn" onClick={fetchData}>Start quiz</button>
-                </section>
-                <p>Made with ❤️ by Obinna-Dev</p>
-                {/* <Questions /> */}
+                <Start handleChange={handleChange} formData={formData} fetchData={fetchData}/>
+                <Questions />
             </div>
         </main>
     )
---
+++ src/components/Start.js
@@ -0,0 +1,61 @@
+export default function Start(props) {
+    return (
+        <section className="start-section">
+            <div className="start-container">
+                    <div className="header-image">
+                        <h1>Obinna's Trivia Quiz</h1>
+                        <p>Fun trivia games</p>
+                    </div>
+                    
+                    <form>
+                        <select 
+                            id="quizCategory"
+                            value={props.formData.quizCategory}
+                            onChange={props.handleChange}
+                            name="quizCategory"
+                        >
+                            <option value="">-- Choose category --</option>
+                            <option value="9">General Knowledge</option>
+                            <option value="10">Entertainment: Books</option>
+                            <option value="11">Entertainment: Film</option>
+                            <option value="12">Entertainment: Music</option>
+                            <option value="13">Entertainment: Musicals and Theatres</option>
+                            <option value="14">Entertainment: Television</option>
+                            <option value="15">Video Games</option>
+                            <option value="16">Board Games</option>
+                            <option value="17">Science and Nature</option>
+                            <option value="18">Science: Computers</option>
+                            <option value="19">Science: Mathematics</option>
+                            <option value="20">Mythology</option>
+                            <option value="21">Sports</option>
+                            <option value="22">Geography</option>
+                            <option value="23">History</option>
+                            <option value="24">Politics</option>
+                            <option value="25">Arts</option>
+                            <option value="26">Celebrities</option>
+                            <option value="27">Animals</option>
+                            <option value="28">Vehicles</option>
+                            <option value="29">Entertainment: Comics</option>
+                            <option value="30">Science: Gadgets</option>
+                            <option value="31">Entertainment: Japanese Anime and Manga</option>
+                            <option value="32">Entertainment: Cartoon and Animations</option>
+                        </select>
+                        <br />
+                        <select 
+                            id="quizDifficulty"
+                            value={props.formData.quizDifficulty}
+                            onChange={props.handleChange}
+                            name="quizDifficulty"
+                        >
+                            <option value="">-- Choose difficulty --</option>
+                            <option value="easy">Easy 😃</option>
+                            <option value="medium">Medium 😅</option>
+                            <option value="hard">Hard 🥵</option>
+                        </select>
+                    </form>
+                    <button className="start-btn" onClick={props.fetchData}>Start quiz</button>
+            </div>
+            <p>Made with ❤️ by Obinna-Dev</p>
+        </section>
+    )
+}
